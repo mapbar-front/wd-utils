0baf439d05faa7faae5b7720d1210a1d985baac0
驭舟 <li.lwd@alibaba-inc.com>
2020-08-15 15:33:04 +0800

    feat: 🎸 工具库的建设

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import storage from './packages/storage/index'
 import validate from './packages/validate/index'
 import formatDateTime from './packages/format/index'
+import calculate from './packages/calculate/index'
 
 export {
   storage,
   validate,
-  formatDateTime
+  formatDateTime,
+  calculate
 }
